69c5033f38512fca3034e050ae3cdf9cb73776f6
Martyn Jacques <martynjacques@gmail.com>
2016-04-12 22:50:00 +0100

    Horrible, messy implementation. Seems to work. Needs improvement on enum and in play selection

--- bet.py
+++ bet.py
@@ -24,20 +24,62 @@ def not_negative(profit_list):
 
 def get_refund(index, stake_list):
     if index == 0:
-        min_of_two_pre_match = min(stake_list[1:])
-        if min_of_two_pre_match > 50:
-            min_of_two_pre_match = 50
-        return [0, min_of_two_pre_match, min_of_two_pre_match]
+        max_of_pre_match = max([stake_list[1], stake_list[2]])
+        if stake_list[0] > 50 and stake_list[1] > 50 and stake_list[2] > 50:
+            return_value = 50
+        elif stake_list[0] < 50 and max_of_pre_match > stake_list[0]:
+            return_value = stake_list[0]
+        elif stake_list[0] < 50 and max_of_pre_match < stake_list[0]:
+            return_value = max_of_pre_match
+        else:
+            if max_of_pre_match < 50:
+                return_value = max_of_pre_match
+            else:
+                return_value = 50
+        return [0, return_value, return_value]
     elif index == 1:
-        min_of_two_pre_match = min([stake_list[0], stake_list[2]])
-        if min_of_two_pre_match > 50:
-            min_of_two_pre_match = 50
-        return [min_of_two_pre_match, 0, min_of_two_pre_match]
+        max_of_pre_match = max([stake_list[0], stake_list[2]])
+        if stake_list[1] > 50 and stake_list[0] > 50 and stake_list[2] > 50:
+            return_value = 50
+        elif stake_list[1] < 50 and max_of_pre_match > stake_list[1]:
+            return_value = stake_list[1]
+        elif stake_list[1] < 50 and max_of_pre_match < stake_list[1]:
+            return_value = max_of_pre_match
+        else:
+            if max_of_pre_match < 50:
+                return_value = max_of_pre_match
+            else:
+                return_value = 50
+        return [return_value, 0, return_value]
     else:
-        min_of_two_pre_match = min(stake_list[:1])
-        if min_of_two_pre_match > 50:
-            min_of_two_pre_match = 50
-        return [min_of_two_pre_match, min_of_two_pre_match, 0]
+        max_of_pre_match = max([stake_list[0], stake_list[1]])
+        if stake_list[2] > 50 and stake_list[0] > 50 and stake_list[1] > 50:
+            return_value = 50
+        elif stake_list[2] < 50 and max_of_pre_match > stake_list[2]:
+            return_value = stake_list[1]
+        elif stake_list[2] < 50 and max_of_pre_match < stake_list[2]:
+            return_value = max_of_pre_match
+        else:
+            if max_of_pre_match < 50:
+                return_value = max_of_pre_match
+            else:
+                return_value = 50
+        return [return_value, return_value, 0]
+
+def print_table(odds,
+                stake,
+                returns,
+                refund,
+                total_returns,
+                total_outlay,
+                profit_list):
+    results = ["Home", "Draw", "Away"]
+    print "Result \t Odds \t Stake \t Ret \t Ref \t TRet \t TOut \t Profit"
+    for a in range(0, 3):
+        print "{0} \t {1} \t {2} \t {3} \t {4} \t {5} \t {6} \t {7}".format(results[a], odds[a], stake[a], returns[a], refund[a], total_returns[a], total_outlay[a], profit_list[a])
+
+
+    # print "In play bet: {0}".format(map_to_result(in_play_index))
 
 def print_stake_and_profit(stakes, profits, in_play_index):
     results = ["Home", "Draw", "Away"]
@@ -51,21 +93,37 @@ def main():
     odds = map(float, sys.argv[1:])
     index = odds.index(max(odds))
 
-    best_profit = []
-    best_stake = []
-    best_total = 0
+    best_equal_stake = []
+    best_equal_profit = []
+    best_equal_returns = []
+    best_equal_refund = []
+    best_equal_total_returns = []
+    best_equal_total_outlay = []
+    best_equal_total_profit = 0
 
     best_home_win_profit = 0
-    best_home_win_profits = []
     best_home_win_stake = []
+    best_home_win_profit_list = []
+    best_home_win_returns = []
+    best_home_win_refund = []
+    best_home_win_total_returns = []
+    best_home_win_total_outlay = []
 
     best_draw_profit = 0
-    best_draw_profits = []
     best_draw_stake = []
+    best_draw_profit_list = []
+    best_draw_returns = []
+    best_draw_refund = []
+    best_draw_total_returns = []
+    best_draw_total_outlay = []
 
     best_away_win_profit = 0
-    best_away_win_profits = []
     best_away_win_stake = []
+    best_away_win_profit_list = []
+    best_away_win_returns = []
+    best_away_win_refund = []
+    best_away_win_total_returns = []
+    best_away_win_total_outlay = []
 
     for a in range(10,110):
         for b in range(10,110):
@@ -88,42 +146,80 @@ def main():
 
                 if not_negative(overall_profit):
                     difference = get_difference(overall_profit)
-                    if difference <= 3 and sum(overall_profit) > best_total:
-                        # print "Even profits: {0} Total: {1}".format(overall_profit, sum(overall_profit))
-                        # print "With Stakes: {0} Total {1}".format(stake, sum(stake))
-                        best_profit = overall_profit
-                        best_stake = stake
-                        best_total = sum(best_profit)
+                    if difference <= 3 and sum(overall_profit) > best_equal_total_profit:
+                        best_equal_stake = stake
+                        best_equal_profit = overall_profit
+                        best_equal_returns = returns
+                        best_equal_refund = refund
+                        best_equal_total_returns = total_returns
+                        best_equal_total_outlay = total_outlay
+                        best_equal_total_profit = sum(best_equal_profit)
 
                     # Home Win
                     if overall_profit[0] > best_home_win_profit and overall_profit[1] > 8 and overall_profit[2] > 8:
                         best_home_win_profit = overall_profit[0]
-                        best_home_win_profits = overall_profit
                         best_home_win_stake = stake
+                        best_home_win_profit_list = overall_profit
+                        best_home_win_returns = returns
+                        best_home_win_refund = refund
+                        best_home_win_total_returns = total_returns
+                        best_home_win_total_outlay = total_outlay
 
                     # Draw
                     if overall_profit[1] > best_draw_profit and overall_profit[0] > 8 and overall_profit[2] > 8:
                         best_draw_profit = overall_profit[1]
-                        best_draw_profits = overall_profit
                         best_draw_stake = stake
+                        best_draw_profit_list = overall_profit
+                        best_draw_returns = returns
+                        best_draw_refund = refund
+                        best_draw_total_returns = total_returns
+                        best_draw_total_outlay = total_outlay
 
                     # Away Win
                     if overall_profit[2] > best_away_win_profit and overall_profit[0] > 8 and overall_profit[1] > 8:
                         best_away_win_profit = overall_profit[2]
-                        best_away_win_profits = overall_profit
                         best_away_win_stake = stake
+                        best_away_win_profit_list = overall_profit
+                        best_away_win_returns = returns
+                        best_away_win_refund = refund
+                        best_away_win_total_returns = total_returns
+                        best_away_win_total_outlay = total_outlay
 
     print "\nBest equal profit"
-    print_stake_and_profit(best_stake, best_profit, index)
+    print_table(odds,
+                best_equal_stake,
+                best_equal_returns,
+                best_equal_refund,
+                best_equal_total_returns,
+                best_equal_total_outlay,
+                best_equal_profit)
 
     print "\nBest home win"
-    print_stake_and_profit(best_home_win_stake, best_home_win_profits, index)
+    print_table(odds,
+                best_home_win_stake,
+                best_home_win_returns,
+                best_home_win_refund,
+                best_home_win_total_returns,
+                best_home_win_total_outlay,
+                best_home_win_profit_list)
 
     print "\nBest draw"
-    print_stake_and_profit(best_draw_stake, best_draw_profits, index)
+    print_table(odds,
+                best_draw_stake,
+                best_draw_returns,
+                best_draw_refund,
+                best_draw_total_returns,
+                best_draw_total_outlay,
+                best_draw_profit_list)
 
     print "\nBest away win"
-    print_stake_and_profit(best_away_win_stake, best_away_win_profits, index)
+    print_table(odds,
+                best_away_win_stake,
+                best_away_win_returns,
+                best_away_win_refund,
+                best_away_win_total_returns,
+                best_away_win_total_outlay,
+                best_away_win_profit_list)
 
 
 if __name__ == "__main__":
