89582f014b7c22b2e3914fff624bce24682a51ea
Martyn Jacques <mjacques@uk.ibm.com>
2016-04-20 13:12:45 +0100

    better WHEN bets

--- bet.py
+++ bet.py
@@ -58,6 +58,8 @@ class MatchedBet(object):
                 self.profits[a]
             )
         table += "\nIn play bet: {0}".format(self.get_in_play_bet())
+        table += "\nB365 pre-match bet: {0}".format(self.get_bet_365_pre_match_bet())
+        table += "\nOther pre-match bet: {0}".format(self.get_other_pre_match_bet())
         return table
 
     def get_in_play_bet(self):
@@ -71,51 +73,43 @@ class MatchedBet(object):
             + (sorted_list[1] - sorted_list[0])
         )
 
+    def get_bet_365_pre_match_bet(self):
+        pre_match_bets = list(self.stakes)
+        del pre_match_bets[self.refunds.index(0)]
+        if sum(1 for n in pre_match_bets if n > 50) == 2:
+            return "Any"
+        max_of_pre_match = max(pre_match_bets)
+        return self.teams[self.stakes.index(max_of_pre_match)]
+
+    def get_other_pre_match_bet(self):
+        pre_match_bets = list(self.stakes)
+        del pre_match_bets[self.refunds.index(0)]
+        if sum(1 for n in pre_match_bets if n > 50) == 2:
+            return "Any"
+        min_of_pre_match = min(pre_match_bets)
+        return self.teams[self.stakes.index(min_of_pre_match)]
+
     def get_refund(self, index, stake_list):
-        if index == 0:
-            max_of_pre_match = max([stake_list[1], stake_list[2]])
-            if stake_list[0] > 50 and stake_list[1] > 50 and stake_list[2] > 50:
-                return_value = 50
-            elif stake_list[0] < 50 and max_of_pre_match > stake_list[0]:
-                return_value = stake_list[0]
-            elif stake_list[0] < 50 and max_of_pre_match < stake_list[0]:
-                return_value = max_of_pre_match
-            else:
-                if max_of_pre_match < 50:
-                    return_value = max_of_pre_match
-                else:
-                    return_value = 50
-            return [0, return_value, return_value]
+        pre_match_bets = list(stake_list)
+        del pre_match_bets[index]
+        in_play_bet_value = stake_list[index]
+        max_of_pre_match = max(pre_match_bets)
+        if sum(1 for n in stake_list if n > 50) == 3:
+            refund_value = 50
+        elif in_play_bet_value < 50 and max_of_pre_match > in_play_bet_value:
+            refund_value = in_play_bet_value
+        elif in_play_bet_value < 50 and max_of_pre_match < in_play_bet_value:
+            refund_value = max_of_pre_match
+        elif max_of_pre_match < 50:
+            refund_value = max_of_pre_match
+        else:
+            refund_value = 50
 
-        if index == 1:
-            max_of_pre_match = max([stake_list[0], stake_list[2]])
-            if stake_list[1] > 50 and stake_list[0] > 50 and stake_list[2] > 50:
-                return_value = 50
-            elif stake_list[1] < 50 and max_of_pre_match > stake_list[1]:
-                return_value = stake_list[1]
-            elif stake_list[1] < 50 and max_of_pre_match < stake_list[1]:
-                return_value = max_of_pre_match
-            else:
-                if max_of_pre_match < 50:
-                    return_value = max_of_pre_match
-                else:
-                    return_value = 50
-            return [return_value, 0, return_value]
-
-        if index == 2:
-            max_of_pre_match = max([stake_list[0], stake_list[1]])
-            if stake_list[2] > 50 and stake_list[0] > 50 and stake_list[1] > 50:
-                return_value = 50
-            elif stake_list[2] < 50 and max_of_pre_match > stake_list[2]:
-                return_value = stake_list[1]
-            elif stake_list[2] < 50 and max_of_pre_match < stake_list[2]:
-                return_value = max_of_pre_match
-            else:
-                if max_of_pre_match < 50:
-                    return_value = max_of_pre_match
-                else:
-                    return_value = 50
-            return [return_value, return_value, 0]
+        refund_list = [0, 0, 0]
+        for i in range(0,3):
+            if i != index:
+                refund_list[i] = refund_value
+        return refund_list
 
     def has_negative(self):
         if sum(1 for n in self.profits if n < 0) == 0:
@@ -138,6 +132,7 @@ def main():
     teams = [sys.argv[1], sys.argv[2]]
     odds = [float(x) for x in sys.argv[3:]]
 
+    # Initialise the bets
     best_equal_bet = MatchedBet(teams, odds, [1, 1, 1])
     best_home_win_bet = MatchedBet(teams, odds, [1, 1, 1])
     best_draw_bet = MatchedBet(teams, odds, [1, 1, 1])
