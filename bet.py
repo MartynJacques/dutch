bd26ada83565f15845a92c1f641ad871202364db
Martyn Jacques <mjacques@uk.ibm.com>
2016-04-20 13:46:44 +0100

    better refunds

--- bet.py
+++ bet.py
@@ -9,16 +9,17 @@ class MatchedBet(object):
     """
         Data structure to hold the contents of a bet.
     """
-    def __init__(self, teams, odds, stakes):
+    def __init__(self, teams, odds, stakes, bet_365_pre_match_bet):
         self.teams = [teams[0], "Draw", teams[1]]
         self.odds = odds
         self.stakes = stakes
+        self.bet_365_pre_match_bet_index = self.teams.index(
+            bet_365_pre_match_bet
+        )
         self.returns = [
             self.stakes[i] * self.odds[i] for i in range(len(stakes))
         ]
-        self.refunds = self.get_refund(
-            self.odds.index(max(self.odds)), self.stakes
-        )
+        self.refunds = self.get_refund()
         self.total_returns = [
             self.returns[i] + self.refunds[i] for i in range(len(self.refunds))
         ]
@@ -31,7 +32,7 @@ class MatchedBet(object):
 
     def __str__(self):
         row_format = (
-            "{0:10} {1:>6} {2:>5} {3:>8} {4:>6} {5:>13} {6:>13} {7:>6}"
+            "{0:10} {1:>6} {2:>5} {3:>8} {4:>6} {5:>13} {6:>13} {7:>6} {8:10}"
         )
         table = row_format.format(
             "Result",
@@ -41,7 +42,8 @@ class MatchedBet(object):
             "Refund",
             "Total Returns",
             "Total Outlay",
-            "Profit"
+            "Profit",
+            "When"
         )
         for a in range(0, 3):
             table += '\n'
@@ -55,15 +57,10 @@ class MatchedBet(object):
                 self.refunds[a],
                 self.total_returns[a],
                 self.outlay,
-                self.profits[a]
+                self.profits[a],
+                self.get_when()[a]
             )
-        table += "\nIn play bet: {0}".format(self.get_in_play_bet())
-        table += "\nB365 pre-match bet: {0}".format(self.get_bet_365_pre_match_bet())
-        table += "\nOther pre-match bet: {0}".format(self.get_other_pre_match_bet())
         return table
-
-    def get_in_play_bet(self):
-        return self.teams[self.refunds.index(0)]
 
     def get_difference(self):
         sorted_list = sorted(self.profits, key=float)
@@ -73,41 +70,35 @@ class MatchedBet(object):
             + (sorted_list[1] - sorted_list[0])
         )
 
-    def get_bet_365_pre_match_bet(self):
-        pre_match_bets = list(self.stakes)
-        del pre_match_bets[self.refunds.index(0)]
-        if sum(1 for n in pre_match_bets if n > 50) == 2:
-            return "Any"
-        max_of_pre_match = max(pre_match_bets)
-        return self.teams[self.stakes.index(max_of_pre_match)]
+    def get_when(self):
+        when = ["Never", "Never", "Never"]
+        when[self.get_in_play_bet_index()] = "Bet365 in play"
+        when[self.get_other_pre_match_bet()] = "Other pre-match"
+        when[self.bet_365_pre_match_bet_index] = "Bet365 pre-match"
+        return when
+
+    def get_in_play_bet_index(self):
+        return self.odds.index(max(self.odds))
 
     def get_other_pre_match_bet(self):
-        pre_match_bets = list(self.stakes)
-        del pre_match_bets[self.refunds.index(0)]
-        if sum(1 for n in pre_match_bets if n > 50) == 2:
-            return "Any"
-        min_of_pre_match = min(pre_match_bets)
-        return self.teams[self.stakes.index(min_of_pre_match)]
+        for x in range(0,3):
+            if (x != self.get_in_play_bet_index()
+                    and x != self.bet_365_pre_match_bet_index):
+                return x
 
-    def get_refund(self, index, stake_list):
-        pre_match_bets = list(stake_list)
-        del pre_match_bets[index]
-        in_play_bet_value = stake_list[index]
-        max_of_pre_match = max(pre_match_bets)
-        if sum(1 for n in stake_list if n > 50) == 3:
+    def get_refund(self):
+        in_play_index = self.get_in_play_bet_index()
+        bet_365_pre_match_stake = self.stakes[self.bet_365_pre_match_bet_index]
+        in_play_stake = self.stakes[in_play_index]
+
+        if bet_365_pre_match_stake > 50 and in_play_stake > 50:
             refund_value = 50
-        elif in_play_bet_value < 50 and max_of_pre_match > in_play_bet_value:
-            refund_value = in_play_bet_value
-        elif in_play_bet_value < 50 and max_of_pre_match < in_play_bet_value:
-            refund_value = max_of_pre_match
-        elif max_of_pre_match < 50:
-            refund_value = max_of_pre_match
         else:
-            refund_value = 50
+            refund_value = min([bet_365_pre_match_stake, in_play_stake])
 
         refund_list = [0, 0, 0]
         for i in range(0,3):
-            if i != index:
+            if i != in_play_index:
                 refund_list[i] = refund_value
         return refund_list
 
@@ -129,23 +120,37 @@ class MatchedBet(object):
         return self.profits[2]
 
 def main():
-    teams = [sys.argv[1], sys.argv[2]]
-    odds = [float(x) for x in sys.argv[3:]]
+    try:
+        teams = [sys.argv[1], sys.argv[2]]
+        odds = [float(x) for x in sys.argv[3:6]]
+        bet_365_pre_match_bet = sys.argv[6]
+    except IndexError as e:
+        print "Not enough arguments."
+        print(
+            "To run the program type 'python bet.py HOME_TEAM AWAY_TEAM '"
+            "HOME_TEAM_ODDS DRAW_ODDS AWAY_TEAM_ODDS BET_365_PRE_MATCH_BET'"
+        )
+        exit(1)
 
     # Initialise the bets
-    best_equal_bet = MatchedBet(teams, odds, [1, 1, 1])
-    best_home_win_bet = MatchedBet(teams, odds, [1, 1, 1])
-    best_draw_bet = MatchedBet(teams, odds, [1, 1, 1])
-    best_away_win_bet = MatchedBet(teams, odds, [1, 1, 1])
+    best_equal_bet = MatchedBet(teams, odds, [1, 1, 1], bet_365_pre_match_bet)
+    best_home_win_bet = MatchedBet(teams, odds, [1, 1, 1], bet_365_pre_match_bet)
+    best_draw_bet = MatchedBet(teams, odds, [1, 1, 1], bet_365_pre_match_bet)
+    best_away_win_bet = MatchedBet(teams, odds, [1, 1, 1], bet_365_pre_match_bet)
 
     for a in range(10, 110):
         for b in range(10, 110):
             for c in range(10, 110):
-                this_bet = MatchedBet(teams, odds, [a, b, c])
+                this_bet = MatchedBet(
+                    teams,
+                    odds,
+                    [a, b, c],
+                    bet_365_pre_match_bet
+                )
 
                 if not this_bet.has_negative():
                     # Best equal profit
-                    if (this_bet.get_difference() <= 3
+                    if (this_bet.get_difference() <= 2
                             and this_bet.sum_of_profits()
                                 > best_equal_bet.sum_of_profits()):
                         best_equal_bet = this_bet
