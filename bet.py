4f875f360b7cb39e651d1c3c74db69e86667e91a
Martyn Jacques <martynjacques@gmail.com>
2017-04-11 11:45:28 +0100

    refactor and fixed chosing the pre match bet

--- bet.py
+++ bet.py
@@ -5,6 +5,9 @@
 
 import sys
 
+MIN_RETURNS = 1
+
+
 class MatchedBet(object):
     """
         Data structure to hold the contents of a bet.
@@ -68,7 +71,11 @@ class MatchedBet(object):
         return when
 
     def get_in_play_bet_index(self):
-        return self.odds.index(max(self.odds))
+        y = [
+            self.odds[self.get_bet_365_pre_match_bet_index()]
+        ]
+        without_inplay = [item for item in self.odds if item not in y]
+        return self.odds.index(min(without_inplay))
 
     def get_bet_365_pre_match_bet_index(self):
         return self.teams.index(
@@ -76,9 +83,9 @@ class MatchedBet(object):
         )
 
     def get_other_pre_match_bet(self):
-        for x in range(0,3):
-            if (x != self.get_in_play_bet_index()
-                    and x != self.get_bet_365_pre_match_bet_index()):
+        for x in range(0, 3):
+            if (x != self.get_in_play_bet_index() and
+                    x != self.get_bet_365_pre_match_bet_index()):
                 return x
 
     def get_refund(self):
@@ -94,7 +101,7 @@ class MatchedBet(object):
             refund_value = min(bet_365_pre_match_stake, in_play_stake)
 
         refund_list = [0, 0, 0]
-        for i in range(0,3):
+        for i in range(0, 3):
             if i != in_play_index:
                 refund_list[i] = refund_value
         return refund_list
@@ -119,13 +126,39 @@ class MatchedBet(object):
     def has_equal_returns(self):
         sorted_list = sorted(self.profits, key=float)
         difference = (
-            (sorted_list[2] - sorted_list[1])
-            + (sorted_list[2] - sorted_list[0])
-            + (sorted_list[1] - sorted_list[0])
+            (sorted_list[2] - sorted_list[1]) +
+            (sorted_list[2] - sorted_list[0]) +
+            (sorted_list[1] - sorted_list[0])
         )
         if difference < 3:
             return True
         return False
+
+    def has_better_equal_profit_than(self, other_bet):
+        return (self.has_equal_returns() and
+                self.sum_of_profits() > other_bet.sum_of_profits())
+
+    def has_better_home_win_profit_than(self, other_bet):
+        return (
+            self.home_win_profit() > other_bet.home_win_profit() and
+            self.draw_profit() > MIN_RETURNS and
+            self.away_win_profit() > MIN_RETURNS
+        )
+
+    def has_better_draw_profit_than(self, other_bet):
+        return (
+            self.draw_profit() > other_bet.draw_profit() and
+            self.home_win_profit() > MIN_RETURNS and
+            self.away_win_profit() > MIN_RETURNS
+        )
+
+    def has_better_away_win_profit_than(self, other_bet):
+        return (
+            self.away_win_profit() > other_bet.away_win_profit() and
+            self.home_win_profit() > MIN_RETURNS and
+            self.draw_profit() > MIN_RETURNS
+        )
+
 
 def main():
     try:
@@ -135,8 +168,8 @@ def main():
     except IndexError as e:
         print "Not enough arguments."
         print(
-            "To run the program type 'python bet.py HOME_TEAM AWAY_TEAM '"
-            "HOME_TEAM_ODDS DRAW_ODDS AWAY_TEAM_ODDS BET_365_PRE_MATCH_BET'"
+            "To run the program type 'python bet.py HOME_TEAM AWAY_TEAM "
+            "HOME_TEAM_ODDS DRAW_ODDS AWAY_TEAM_ODDS BET_365_PRE_MATCH_BET"
         )
         exit(1)
 
@@ -154,9 +187,9 @@ def main():
         teams, odds, [1, 1, 1], bet_365_pre_match_bet
     )
 
-    for a in range(10, 110):
-        for b in range(10, 110):
-            for c in range(10, 110):
+    for a in range(10, 200):
+        for b in range(10, 200):
+            for c in range(10, 200):
                 this_bet = MatchedBet(
                     teams,
                     odds,
@@ -164,32 +197,24 @@ def main():
                     bet_365_pre_match_bet
                 )
 
-                if not this_bet.has_negative():
-                    # Best equal profit
-                    if (this_bet.has_equal_returns()
-                            and this_bet.sum_of_profits()
-                                > best_equal_bet.sum_of_profits()):
-                        best_equal_bet = this_bet
+                if this_bet.has_negative():
+                    continue
 
-                    # Home Win
-                    if (this_bet.home_win_profit()
-                            > best_home_win_bet.home_win_profit()
-                                and this_bet.draw_profit() > 8
-                                    and this_bet.away_win_profit() > 8):
-                        best_home_win_bet = this_bet
+                # Best equal profit
+                if this_bet.has_better_equal_profit_than(best_equal_bet):
+                    best_equal_bet = this_bet
 
-                    # Draw
-                    if (this_bet.draw_profit() > best_draw_bet.draw_profit()
-                            and this_bet.home_win_profit() > 8
-                                and this_bet.away_win_profit() > 8):
-                        best_draw_bet = this_bet
+                # Home Win
+                if this_bet.has_better_home_win_profit_than(best_home_win_bet):
+                    best_home_win_bet = this_bet
 
-                    # Away Win
-                    if (this_bet.away_win_profit()
-                            > best_away_win_bet.away_win_profit()
-                                and this_bet.home_win_profit() > 8
-                                    and this_bet.draw_profit() > 8):
-                        best_away_win_bet = this_bet
+                # Draw
+                if this_bet.has_better_draw_profit_than(best_draw_bet):
+                    best_draw_bet = this_bet
+
+                # Away Win
+                if this_bet.has_better_away_win_profit_than(best_away_win_bet):
+                    best_away_win_bet = this_bet
 
     print "\nBest equal profit"
     print best_equal_bet
